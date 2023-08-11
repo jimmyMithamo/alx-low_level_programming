87d1b8958638d584eea2aafec2d0ccb7647c96ae
JimmyMithamo <j1mithamo@gmail.com>
2023-08-11 14:56:06 +0300

    adding the file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  *main - the function prints all number of base 10 starting from 0
+  *Return: returns an integer 0
+ */
+
+int main()
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	putchar('\n');
+	return (0);
+} 
