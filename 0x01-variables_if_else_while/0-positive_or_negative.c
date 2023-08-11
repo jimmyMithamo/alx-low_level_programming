67d8469ef6592de2236ac903c6635f5878f0cb3e
JimmyMithamo <j1mithamo@gmail.com>
2023-08-11 13:50:04 +0300

    adding the file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,14 +3,15 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/**main-function assigns a random number and tests whether its
+/*main-function assigns a random number and tests whether its
   *greater than zero or less than zero
-  *Return: there is no return value.
+  *Return: there is no return value
  */
 int main(void)
 {
 	int n;
 	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
