21811d29418ac8bc73fcaa343aa73a8710914748
JimmyMithamo <j1mithamo@gmail.com>
2023-08-11 14:47:16 +0300

    adding the file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  *main - this function print the alphabets skipping letters q and e
+  *Return:no return value-function is void 
+  */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == 'q' || letter == 'e')
+			continue;
+		else
+			putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
