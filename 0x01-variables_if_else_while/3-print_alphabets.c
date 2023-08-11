6ed8695ec39d4bf327579b0e74581e6946a1531f
JimmyMithamo <j1mithamo@gmail.com>
2023-08-11 14:32:45 +0300

    adding the file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main -this function prints alphabets a-z in lower case
+ *and then the uppercase letters
+ *only the putchar  function has been used
+ *Return:the function returns 0 since its a void function
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+        for (letter = 'A'; letter <= 'Z'; letter++)
+        {
+		putchar(letter);
+	}
+	putchar('\n');
+        return (0);
+}
