ea238cab81e767508aec8b582ada010a8c7f9b52
jimmyMithamo <j1mithamo@gmail.com>
2023-08-15 22:25:28 +0300

    adding file

---
+++ 0x02-functions_nested_loops/add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * add - Adds two intagers provide
+  * @n1: first integer
+  * @n2: second integer
+  *
+  * Return: sum
+  */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
