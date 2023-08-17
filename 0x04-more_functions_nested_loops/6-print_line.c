f1fc4c51094b86e0b0fdd877a4e8b1aef517dc5f
jimmyMithamo <j1mithamo@gmail.com>
2023-08-17 14:51:36 +0300

    adding the source code to github

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
-  *print_line - prnts line using -
-  *_putchar - prints the character to the console
-  *@n: integer that determines size of line
-  *Return: void
-  */
+ * print_line - printing lines
+ * @n: number of times
+ *
+ * Return: void
+ */
+
 
 void print_line(int n)
 {
 	int i;
-	
+
 	for (i = 0; i < n; i++)
 	{
 		_putchar('_');
 	}
 	_putchar('\n');
-
 }
