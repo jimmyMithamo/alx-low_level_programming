285ec72b41e91676d85e65281a03494dff860c22
jimmyMithamo <j1mithamo@gmail.com>
2023-08-17 17:39:29 +0300

    adding the source code to github

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,14 @@
 #include "main.h"
 
 /**
-*print_numbers - print 0123456789
-* owned by Bwave ICT
-*Return: void
-*/
-
-void print_numbers(void)
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
+ * Return: void
+ */
+void  print_numbers(void)
 {
-	char c;
+	int i;
 
-	for (c = '0'; c <= '9'; c++)
-	{
-	_putchar(c);
-	}
+	for (i = 0; i < 10; i++)
+		_putchar(i + '0');
 	_putchar('\n');
 }
