31417aad5ec11b844602adbb8946e66b18f19c79
jimmyMithamo <j1mithamo@gmail.com>
2023-08-17 12:30:40 +0300

    adding the source code to github

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**mul - function to multiply two numbers
+  *
+  *@a: first number
+  *@b: second number
+  *Return: always 0
+  */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	putchar(result);
+	return (0);
+}
+
