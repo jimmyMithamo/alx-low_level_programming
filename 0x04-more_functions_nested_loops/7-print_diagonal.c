3ca02adcb53f527bfb1cc7a3718d1d9c6c8dbcec
jimmyMithamo <j1mithamo@gmail.com>
2023-08-17 15:16:06 +0300

    adding the source code to github

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,9 +11,6 @@
 void print_diagonal(int n)
 {
 	int i, j;
-
-	if (n <= 0)
-		_putchar('\n');
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < i; j++)
@@ -23,5 +20,6 @@ void print_diagonal(int n)
 		_putchar('\\');
 		_putchar('\n');
 	}
-
+	_putchar('\n');
 }
+
