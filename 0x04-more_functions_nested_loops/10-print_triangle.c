45733f20bbb61a0c86d8cb5aa7b49e2e2b9fa8c6
jimmyMithamo <j1mithamo@gmail.com>
2023-08-17 16:45:48 +0300

    adding the source code to github

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  *print_triangle - prints a triangle using symbol #
+  *@size:size of the triangle
+  *Return:void
+  */
+
+void print_triangle(void)
+{
+	int i, j, k;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size - i; j++)
+			_putchar(' ');
+		for (k = 1; k <= i; k++)
+			_putchar('#');
+		_putchar('\n');
+	}
+	(size <= 0) ? _putchar('\n') : 0;
+}
