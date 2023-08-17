e22fb4d42aca9e21b83a3e667dcb5c794a57129c
jimmyMithamo <j1mithamo@gmail.com>
2023-08-17 14:11:46 +0300

    adding the source code to github

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,8 +15,14 @@ void more_numbers(void)
 		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
-			_putchar('1');
-			_putchar(j % 10 + '0');
+			{
+				_putchar('1');
+				_putchar(j % 10 + '0');
+			}
+			else
+			{
+				_putchar(j);
+			}
 		}
 		_putchar('\n');
 	}
