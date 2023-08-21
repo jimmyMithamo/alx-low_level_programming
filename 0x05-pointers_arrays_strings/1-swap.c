b3fae739c82257315ca80157ae37b05ee9370297
jimmyMithamo <j1mithamo@gmail.com>
2023-08-21 21:19:10 +0300

    adding file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  *swap_int- swaps thwo integers
+  *@a:address of first integer
+  *@b:address of second integer
+  *Return:nothing
+  */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	*a = *b;
+	*b = temp;
+}
