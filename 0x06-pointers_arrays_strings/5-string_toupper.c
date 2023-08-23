f87391d08c02b6eda275e6eba535133da8f412a7
jimmyMithamo <j1mithamo@gmail.com>
2023-08-23 21:59:31 +0300

    adding file

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  *string_toupper- converts all elements of a string to upper case
+  *@s:string to be converted
+  *Return:converted string
+  */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			*(s + i) = *(s + i) - 32;
+		}
+		i++;
+	}
+	return (s);
+}
+
