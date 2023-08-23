9cc7ca92aa30de1751f2108a80d9f82f44c985d9
jimmyMithamo <j1mithamo@gmail.com>
2023-08-23 21:41:50 +0300

    adding file

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  *reverse_array - reverses an array
+  *@a:array to be reversed
+  *@n:size of the array
+  *Return:reversed array
+  */
+
+void reverse_array(int *a, int n)
+{
+	int rev, start, end;
+
+	start = 0;
+	end = n-1;
+
+	while (start < end)
+	{
+		rev = *(a + start);
+		*(a + start) = *(a + end);
+		*(a + end) = rev;
+		start++;
+		end--;
+	}
+}
