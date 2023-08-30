1f7a516eea7fcd6e72429a65f3e45aa285238a77
jimmyMithamo <j1mithamo@gmail.com>
2023-08-30 22:08:58 +0300

    source code  files

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  *_pow_recursion - returns thepoer of a digit
+  *@x:value to be raised
+  *@y:power
+  *Return:x raised to power y
+  */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	return (x * _pow_recursion(x, y-1);
+}
