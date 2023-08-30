8d69f3aba8cf5052d4863f3e5ffcf955c80a132b
jimmyMithamo <j1mithamo@gmail.com>
2023-08-30 20:40:22 +0300

    source code  files

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,13 +6,13 @@
   *Return:void
   */
 
-void _puts_recursion(char* s)
+void _puts_recursion(char *s)
 {
 	if (*s == 0)
 	{
 		_putchar('\n');
+		return;
 	}
 	_putchar(*s);
-	_puts_recursion(s+1);
-
+	_puts_recursion(s + 1);
 }
