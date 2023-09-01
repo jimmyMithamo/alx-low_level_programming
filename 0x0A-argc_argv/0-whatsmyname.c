1499b85b6233135e682676b1145000a522b76589
jimmyMithamo <j1mithamo@gmail.com>
2023-09-01 14:49:49 +0300

    adding file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**main - prints the name of this program
+  *@argc:number of arguments provided inthe function
+  *@argv:array of string arguments provided
+  *Return:returns 0 on success and any other number on failure
+  */
+
+int main(int argc, char *argv[])
+{
+	printf("%s \n", argv[0]);
+
+	return (0);
+}
