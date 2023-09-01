8937c0d6a2f2e13ad193c203544685efe4b5c898
jimmyMithamo <j1mithamo@gmail.com>
2023-09-01 15:06:04 +0300

    c source file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *main - prints the number of arguments passed to it
+  *@argc: number of arguments passed to the function
+  *@argv: arguments vector
+  *Return: always 0
+  */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%d\n",argc);
+	return (0);
+}
+  
