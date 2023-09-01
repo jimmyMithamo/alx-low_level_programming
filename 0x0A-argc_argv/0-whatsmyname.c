1d36d5e12c121f12c80fbce94bbf1bc8b460718c
jimmyMithamo <j1mithamo@gmail.com>
2023-09-01 14:58:00 +0300

    correction

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
-/**main - prints the name of this program
-  *@argc:number of arguments provided inthe function
-  *@argv:array of string arguments provided
-  *Return:returns 0 on success and any other number on failure
+/**
+  *main - prints the name of this program
+  *@argc: number of arguments provided inthe function
+  *@argv: array of string arguments provided
+  *Return: returns 0 on success and any other number on failure
   */
 
 int main(int argc, char *argv[])
 {
 	(void) argc;
-
-	printf("%s \n", argv[0]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
